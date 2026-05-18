501b32f50952937a40b8f59e82a5fb3837a32f2c
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-05-18 10:44:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1152,7 +1152,24 @@ for (const c of archivedCampaigns.rows) {
           </tr>
           ${campaignTable || `<tr><td colspan="4">No campaigns yet.</td></tr>`}
         </table>
+<h2>Archived Campaigns</h2>
 
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Advertiser</th>
+    <th>Name</th>
+    <th>Restore</th>
+  </tr>
+
+  ${archivedCampaignTable || `
+    <tr>
+      <td colspan="4">
+        No archived campaigns.
+      </td>
+    </tr>
+  `}
+</table>
         <h2>Assignments</h2>
         <table>
           <tr>
