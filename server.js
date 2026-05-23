e4b7cc1b5fd866b255955e1d00a4f37c4ab6f612
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-05-23 09:30:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3747,42 +3747,46 @@ app.get("/admin/stores", requireLogin, async (req, res) => {
   `));
 });
 app.post("/admin/stores", requireLogin, async (req, res) => {
-
-  const {
-    brand,
-    name,
-    address,
-    inventory_status,
-    maps_url,
-    waze_url
-  } = req.body;
-
-  await q(
-    `
-    INSERT INTO stores (
-      user_id,
+  try {
+    const {
       brand,
       name,
       address,
       inventory_status,
       maps_url,
       waze_url
-    )
-    VALUES ($1,$2,$3,$4,$5,$6,$7)
-    `,
-    [
-      req.session.user.id,
-      brand,
-      name,
-      address,
-      inventory_status,
-      maps_url,
-      waze_url
-    ]
-  );
+    } = req.body;
 
-  res.redirect("/admin/stores");
+    await q(
+      `
+      INSERT INTO stores (
+        user_id,
+        brand,
+        name,
+        address,
+        inventory_status,
+        maps_url,
+        waze_url
+      )
+      VALUES ($1,$2,$3,$4,$5,$6,$7)
+      `,
+      [
+        req.session.user.id,
+        brand,
+        name,
+        address,
+        inventory_status,
+        maps_url,
+        waze_url
+      ]
+    );
+
+    res.redirect("/admin/stores");
+  } catch (err) {
+    res.send("ADD STORE ERROR: " + err.message);
+  }
 });
+    
 app.listen(port, () => {
   console.log("Server running on port " + port);
 });
