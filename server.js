bf4843fa371f2f3b957be0f75caeed887aae0134
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-05-27 13:55:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4552,7 +4552,14 @@ ${detailRows.rows.map(row => `
     <td style="padding:10px;border:1px solid #ddd;">$${Number(row.estimated_revenue).toFixed(2)}</td>
     <td style="padding:10px;border:1px solid #ddd;">--</td>
     <td style="padding:10px;border:1px solid #ddd;">--</td>
-    <td style="padding:10px;border:1px solid #ddd;">--</td>
+   <td style="padding:10px;border:1px solid #ddd;">
+  $${(
+    (
+      proratedCost /
+      Math.max(1, (Number(row.scans) * 20))
+    ) * 1000
+  ).toFixed(2)}
+</td>
   </tr>
 `).join("")}
       </tbody>
