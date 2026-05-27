4a0bf08bd851874b2318c1590b268b95e135302b
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-05-27 10:07:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4462,8 +4462,15 @@ const estimatedRevenue = (totalScans * 5).toFixed(2);
 <div>
   <label>Location</label><br>
   <select name="location_id">
-    <option value="">All Locations</option>
-  </select>
+  <option value="">All Locations</option>
+
+  ${locations.rows.map(location => `
+    <option value="${location.id}">
+      ${location.name}
+    </option>
+  `).join("")}
+
+</select>
 </div>
           <button type="submit">Run Report</button>
         </div>
