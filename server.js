401ce55a462acbcfc23c17c214b674538b9c2191
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-05-30 16:59:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3596,7 +3596,8 @@ app.get("/admin/new-campaign", async (req, res) => {
   ORDER BY email
 `);
 
-  res.send(page("New Campaign", `<div class="topbar"><div class="brand">Vivid Spots</div><h1>Create Campaign</h1></div><div class="wrap"><form method="POST" action="/admin/new-campaign"><div class="formgrid">
+  res.send(page("New Campaign", `<div class="topbar"><div class="brand">Vivid Spots</div><h1>Create Campaign</h1></div><div class="wrap"><form method="POST" action="/admin/new-campaign">
+<div class="formgrid">
   
 ${req.session.user.role === "super_admin" ? `
   <div>
