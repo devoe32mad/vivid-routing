f62adb2273f08791358c96e4052da6673b6f085b
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-06-09 14:35:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2336,7 +2336,7 @@ const roi =
         <tr>
           <td>${r.advertiser || ""}</td>
           <td>${r.campaign_name || ""}</td>
-          <td>TEST123</td>
+          <td>${r.campaign_id || "NO ID"}</td>
          <td style="text-align:center;">${scans}</td>
 <td style="text-align:center;">${offers}</td>
 
