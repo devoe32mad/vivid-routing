6f23742268f58610e6587e3d31aa23bd033feae3
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-06-10 12:07:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2417,8 +2417,9 @@ const roi =
 <td style="text-align:center;">${intentRate}%</td>
 <td style="text-align:center;">${conversions}</td>
 <td style="text-align:center;">${money(conversionValue)}</td>
-          <td>${money(revenue)}</td>
-        <td>${r.allocated_cost ?? "NO FIELD"}</td>
+   <td>${money(revenue)}</td>
+<td>${Number(r.active_days || 0)}</td>
+<td>${money(placementCost)}</td>
 <td>${roi}%</td>
         </tr>
       `;
