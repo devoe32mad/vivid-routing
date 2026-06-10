51acd68072700024a92123b131ca32ffa460778a
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-06-10 12:52:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2442,7 +2442,7 @@ const roi =
 <td style="text-align:center;">${conversions}</td>
 <td style="text-align:center;">${money(conversionValue)}</td>
    <td>${money(revenue)}</td>
-<td>${Number(r.active_days || 0)}</td>
+<td>${JSON.stringify(r)}</td>
 <td>${money(Number(r.allocated_cost || 0))}</td>
 <td>${roi}%</td>
         </tr>
