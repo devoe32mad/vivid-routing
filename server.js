50baae6a1ca3b883f87cc2bdf21241cd70007d85
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-06-10 14:16:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2328,7 +2328,19 @@ COALESCE((
   SELECT SUM(
     GREATEST(
       1,
-      CURRENT_DATE - DATE(COALESCE(qc2.started_at, qc2.assigned_at, CURRENT_TIMESTAMP))
+    LEAST(
+  COALESCE(NULLIF('${endDate}','')::date, CURRENT_DATE),
+  CURRENT_DATE
+)
+-
+GREATEST(
+  DATE(COALESCE(qc2.started_at, qc2.assigned_at, CURRENT_TIMESTAMP)),
+  COALESCE(
+    NULLIF('${startDate}','')::date,
+    DATE(COALESCE(qc2.started_at, qc2.assigned_at, CURRENT_TIMESTAMP))
+  )
+)
++ 1
     )
   )
   FROM qr_campaigns qc2
