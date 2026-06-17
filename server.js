43d1a005dbba53294244acc2269975b06b65d525
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-06-17 11:16:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3650,6 +3650,10 @@ const campaigns = await q(
       <form method="POST" action="/admin/edit-qr/${qr.id}">
         <label>QR Name</label>
         <input name="name" value="${qr.name || ""}" />
+       ${qr.description && qr.description.startsWith("http") ? `
+  <label>Current Destination URL</label>
+  <input value="${qr.description}" readonly />
+` : ""} 
 ${qr.description && qr.description.startsWith("http") ? `
   <label>Current Destination URL</label>
   <input value="${qr.description}" readonly />
