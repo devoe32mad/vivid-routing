739b1726a8b2192a68ef4fd696d7e0a92363c34c
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-06-19 11:46:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4956,7 +4956,7 @@ ${req.session.user.role === "super_admin" ? `
 <div>
   <label>Advertiser</label>
   <input name="advertiser" value="Pepsi" />
-</div><div><label>Campaign Name</label><input name="name" value="Low Inventory Store Push" /></div><div><label>Campaign URL</label><input name="campaign_url" value="https://www.pepsi.com" /></div><div><label>Avg Customer Value</label><input name="avg_customer_value" value="35" /></div><div><label>Conversion Rate (%)</label><input name="conversion_rate" value="10" /></div></div><label><input type="checkbox" name="is_deal_of_day" style="width:auto" /> Deal of the Day</label><br><br><button class="btn" type="submit">Create Campaign</button></form></div>`));
+</div><div><label>Campaign Name</label><input name="name" value="Low Inventory Store Push" /></div><div><label>Campaign URL</label><input name="campaign_url" value="https://www.pepsi.com" /></div><div><label>Avg Customer Value</label><input name="avg_customer_value" value="35" /></div></div><label><input type="checkbox" name="is_deal_of_day" style="width:auto" /> Deal of the Day</label><br><br><button class="btn" type="submit">Create Campaign</button></form></div>`));
 });
 
 app.post("/admin/new-campaign", requireLogin, async (req, res) => {
