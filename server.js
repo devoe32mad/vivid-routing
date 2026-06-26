e1ea4f299a59200974dc9c7b173a997a905960e3
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-06-26 12:38:40 -0400

    Add logging for report generation details

--- server.js
+++ server.js
@@ -3096,7 +3096,9 @@ WHERE c.user_id = $1
         `,
       isSuperAdmin ? [] : [currentUser.id]
     );
-
+console.log("REPORT TITLE:", reportTitle);
+console.log("REPORT ROW COUNT:", reportRows.rows.length);
+console.log("FIRST ROW:", reportRows.rows[0]);
     let reportTable = "";
 
     for (const r of reportRows.rows) {
