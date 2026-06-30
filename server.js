b1a825431a8f787ff3b134af538dcf0247a84aac
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-06-30 11:35:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3577,6 +3577,7 @@ app.get("/reports-qr", requireLogin, async (req, res) => {
   try {
     const currentUser = req.session.user;
     const isSuperAdmin = currentUser.role === "super_admin";
+    console.log("REPORT USER:", currentUser.id, currentUser.email, currentUser.role, "isSuperAdmin:", isSuperAdmin);
     const timeframe = req.query.timeframe || "30";
 const startDate = req.query.start_date || "";
 const endDate = req.query.end_date || "";
