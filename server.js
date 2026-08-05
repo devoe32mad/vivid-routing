caed59b8a8243929dbbc03847bb0f813d107bb46
devoe32mad <96090532+devoe32mad@users.noreply.github.com>
2026-08-05 08:31:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7212,6 +7212,12 @@ app.get("/org-organizations", requireLogin, requireSuperAdmin, async (req, res) 
     res.send("ERROR: " + err.message);
   }
 });
+async function buildOrganizationExportData(
+  req,
+  organizationId,
+  fromDate,
+  toDate
+)
 app.get(
   "/org-organization/:id",
   async (req, res) => {
